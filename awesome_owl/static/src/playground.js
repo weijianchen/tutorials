e9f55eee292c9cce319c579343574e4f35ada459
cwk@macbook <cehn2000@163.com>
2026-04-30 15:50:47 +0800

    convey props to subclass and learn the diff between t-esc and t-out, t-out with markup to render html is powerful, t-esc just to show safe string

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,19 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
+import { Counter } from "./counter/counter.js"; //从当位置开始找要加. 否则/会被认为从根目录开始
+import { Card } from "./card/card.js";
+import { markup } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card }; //把 Counter 注册成 Playground 可以使用的子组件,否则 Owl 不知道 <Counter/> 是什么组件, 就无法在xml中用它
+
+    setup(){
+        this.state = useState({value:0});
+        this.html = "<b>not safe</b>";
+        this.safeHtml = markup("<b>safe content</b>");
+    }
+
+    increment(){
+        this.state.value++;
+    }
 }
