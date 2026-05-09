52c6f415841d3d8cc98dbfc11839db39ad2a03bf
cwk@macbook <cehn2000@163.com>
2026-05-09 12:12:54 +0800

    use slots to create generic card and use t-if to render the content conditionally, now owl tutorial is finished

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,10 +1,21 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
     static template = "awesome_owl.card";
     static props = {
         title: String,
         content1: String,
-        content2: String //定义参数名称及类型，有validation方便开发时检查前端有没有传错、漏传或参数错误
+        content2: String, //定义参数名称及类型，有validation方便开发时检查前端有没有传错、漏传或参数错误
+        slots: {type: Object, optional: true},
+    }
+
+    setup(){
+        this.state = useState({
+            isOpen: true,
+        });
+    }
+
+    toggleContent(){
+        this.state.isOpen = !this.state.isOpen;
     }
 }
