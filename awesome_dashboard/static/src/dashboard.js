22e60b8a8db5a1c348abcc9ec30f1af1c319a0b7
cwk@macbook <cehn2000@163.com>
2026-05-11 17:01:12 +0800

    add buttons and dashboard item and call the server to grab statics

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,14 +1,28 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
-import { useService } from "@web/core/utils/hooks"
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { rpc } from "@web/core/network/rpc";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
 
     setup(){
         this.action = useService("action");
+        this.statics = useState({
+            average_quantity: 0,
+            average_time: 0,
+            nb_cancelled_orders: 0,
+            nb_new_orders: 0,
+            total_amount: 0
+        })
+        onWillStart(async() => {
+            const result = await rpc("/awesome_dashboard/statistics");
+            Object.assign(this.statics, result);
+        })
+
     }
 
     openCustomers(){
